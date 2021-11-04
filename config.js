e1ea8591c9c0ca412d77cac1d823c2552fb1ba2f
thachthao1110 <93698135+thachthao1110@users.noreply.github.com>
2021-11-04 12:50:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Happy birthday",
+    introTitle: 'Dương Ngọc Mẫn',
+    introDesc: `Tui với ông chơi cũng khá thân
+    Tâm sự cũng nhiều
+    Nên giết người diệt khẩu
+    Nói chứ sinh nhật vui vẻ nha má`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    title: 'Phải chăng bạn đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng bạn đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'thích, mà Thảo chơi les rồi <33',
+    btnNo: 'Không nha (chọn này đi:))t không có hứng thú với trai) :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại gặp mình <3',
+    btnReply: 'Tại bạn nợ tui ly sinh tố <3',
+    reply: 'đúng rồi',
+    mess: 'nhớ bao tui 🥰. Yêu bạn nhiều nhiều, quỵt là tui băm 😘😘',
+    messDesc: 'Học kì quân sự nhớ dẫn tui đi chơi, không thì dịp khác, nhất định phải đi.',
+    btnAccept: 'Hứa rồi nha má <3',
+    messLink: 'https://github.com/zukahai/Confess-Crush' //https://www.facebook.com/messages/t/100055822231213
 }
